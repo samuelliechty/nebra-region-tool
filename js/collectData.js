d6b0e8b9588a94c65bbd5615352c547efd9eaf00
samuelliechty <samuelliechty@users.noreply.github.com>
2021-08-27 01:42:36 -0400

    might be a wee bit of broken code

--- js/collectData.js
+++ js/collectData.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+
+console.log("hello world");
+var allData;
+
+(async () => {
+    allData = await csv().fromFile("./countryData.csv");
+    console.log(allData[244]);
+})();
+//max index for json is 244
+
 function hereWeGo() {
     var v = document.getElementById("countryList");
-    document.getElementById("finalValue").innerHTML = v.options[v.selectedIndex].value;
-    document.getElementById("finalString").innerHTML = v.options[v.selectedIndex].text;
-  };
+    var fq;
+    (async () => {
+        allData = await csv().fromFile("./countryData.csv");
+        for(let i = 0; i < 245; i++){
+            if(allData[i].countryCode.toLowerCase() == v.options[v.selectedIndex].value){
+                fq = allData[i].frequency;
+            }
+        }
+        console.log(fq);
+    })();
+};
--- traverseCSV.js
+++ traverseCSV.js
@@ -2,8 +2,24 @@ const fs = require('fs');
 const csv = require('csvtojson');
 
 console.log("hello world");
+var allData;
 
 (async () => {
-    const allData = await csv().fromFile("countryData.csv");
-    console.log(allData);
+    allData = await csv().fromFile("./countryData.csv");
+    console.log(allData[244]);
 })();
+//max index for json is 244
+
+function hereWeGo() {
+    var v = document.getElementById("countryList");
+    var fq;
+    (async () => {
+        allData = await csv().fromFile("./countryData.csv");
+        for(let i = 0; i < 245; i++){
+            if(allData[i].countryCode.toLowerCase() == v.options[v.selectedIndex].value){
+                fq = allData[i].frequency;
+            }
+        }
+        console.log(fq);
+    })();
+};
